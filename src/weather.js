939d7a5a2df30f7932baa9015d33689457d168d6
flippity28 <70317116+flippity28@users.noreply.github.com>
2020-10-23 16:09:59 +0100

    trying out use of loader spinner

--- src/weather.js
+++ src/weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Loader from 'react-loader-spinner'
 
 export default function Weather(){
     
@@ -11,8 +12,8 @@ export default function Weather(){
 
     function handleSubmit(event){event.preventDefault();
      if (city.length > 0) {let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=f5937ab22539bc6268f9a982f0955523`;
-    axios.get(apiUrl).then(showCurrentWeather);} else {alert ("please enter a city")}
-    
+    axios.get(apiUrl).then(showCurrentWeather); } else {alert ("please enter a city")}
+   
       }
 
      function showCurrentWeather(response){
@@ -52,7 +53,15 @@ export default function Weather(){
       </div>
     );
   } else {
-    return form;
+    return <div>{form}
+     <Loader
+         type="Puff"
+         color="white"
+         height={100}
+         width={100}
+         timeout={3000} // 3 secs
+ 
+      /></div>
   }
  
 }
