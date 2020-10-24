c82fb89b2e613052daebfb7d62d7d921809e4b6d
flippity28 <70317116+flippity28@users.noreply.github.com>
2020-10-24 16:19:59 +0100

    decreasing number of components so axios will work
    realised i needed all components condensed so axios works and updates all items

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
 import React from 'react';
 import './App.css';
-import Header from "./header.js";
+import WeatherForecast from "./weatherforecast.js";
 import "./header.css";
 import "./searchlocation.css";
 import WeatherToday from "./weathertoday.js";
 import "./weathertoday.css";
 import FiveDayForecast from "./fivedayforecast.js";
 import "./fivedayforecast.css";
-import MoreInfo from "./moreinfo.js";
-import "./moreinfo.css";
-import UnitConversion from "./unitconversion.js";
-import "./unitconversion.css";
-import Footer from "./footer.js";
-import "./footer.css";
+
+
+
+
 
 export default function  App() {
   return (
     <div className="App container-sm">
-      <Header />
-       <WeatherToday />
-      <FiveDayForecast />
-      <MoreInfo />
-      <UnitConversion />
-      <Footer />
-     
-    </div>
+      <WeatherForecast />
+           </div>
   );
 }
 
--- src/searchlocation.js
+++ src/searchlocation.js
@@ -1,22 +1,128 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
+import Footer from "./footer.js"
+import "./footer.css";
+import UnitConversion from "./unitconversion.js";
+import "./unitconversion.css";
+import MoreInfo from "./moreinfo.js";
+import "./moreinfo.css";
 
 export default function SearchLocation() {
-  return (
+
+  const [city, setCity] = useState("");
+  const [dataLoaded, setDataLoaded] = useState(false)
+
+  function handleSubmit(event){event.preventDefault()
+  if (city.length> 0) {let apiKey = "f5937ab22539bc6268f9a982f0955523"
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayWeather)
+} else{alert("Please search for a city")}}
+
+  function updateCity(event){setCity(event.target.value)}
+
+  function displayWeather (response){console.log(response)
+  setDataLoaded(true)}
+
+  let searchForm =
     <div className="search-location row">
       <div className="col-3"> </div>
-      <form className=" form-group location-search-form col-sm-6">
+      <form className=" form-group location-search-form col-sm-6" onSubmit={handleSubmit}>
         <input
           type="text"
-          id="location-search"
           placeholder="Type a location"
           autoFocus="on"
-          autoComplete="off"
+          autoComplete="off" onChange={updateCity}
         />
         <input type="submit" id="search-button" value="Search" />
-        <button className="my-location text-left" id="current-location">
+       
+      </form> <button className="my-location">
           <i className="fas fa-map-marker-alt"></i>
         </button>
-      </form>
     </div>
-  );
+    
+    if (dataLoaded)
+  
+{return (
+<div> 
+  {searchForm}
+<div class="card-deck row-cols-1">
+        <div class="card col-sm-6">
+             <h5>London</h5>
+            <h6>12:27 Thursday 3rd September</h6>
+            <div  class="card-text row">
+              <i class="fas fa-info today-symbol col-6 justify-content-center"></i>
+                <div class="card-text col-6 justify-content-center">12 °C
+                </div>
+            </div>
+            <div class="row col-12 justify-content-center"> Cloudy</div>
+                  <div class="row col-12 justify-content-center">Feels like 12°C
+                  </div>
+        </div>
+      
+        <div class="card card-today col-sm-6">
+           <h5>Today's Forecast</h5>
+            <div class="card-text todays-forecast">
+              <div> <i class="fas fa-info-circle"></i> Cloudy with some showers</div>
+               <div><i class="fas fa-temperature-high"></i> Highs of 17 °C</div>
+                <div><i class="fas fa-temperature-low"></i> Lows of 10°C</div>
+                <div><i class="fas fa-umbrella"></i> 50% chance of precipitation</div>
+                <div><i class="fas fa-wind"></i> Windspeed 15 km/h </div>
+                <div><i class="fas fa-sun"></i> Sunrise at 05:55</div>
+                <div> <i class="fas fa-moon"></i> Sunset at 20:40</div>
+                
+            </div>
+          </div>
+        </div>
+
+       <div class="card-deck row-cols-1">
+        <div class="card col-sm-6">
+               <h5>Friday 4th</h5>
+               <div  class="card-text">
+                  <i class="fas fa-cloud-rain"></i>
+                    15C</div>
+        </div>
+        <div class="card col-sm-6">
+              <h5>Saturday 5th</h5>
+                <div  class="card-text">
+                  <i class="fas fa-cloud"></i>
+                  17°C
+                </div>
+          </div>
+       
+        <div class="col-sm-6">
+            <h5>Sunday 6th</h5>
+                <div  class="card-text">
+                  <i class="fas fa-cloud-sun"></i>
+                    18°C
+                     
+          </div>
+        </div>
+        <div class="card col-sm-6">
+         
+            <h5>Monday 7th</h5>
+                <div  class="card-text">
+                  <i class="fas fa-sun"></i>
+                    20°C
+                </div>     
+        
+        </div>  
+        <div class="card col-sm-6">
+            <h5>Tuesday 8th</h5>
+                <div  class="card-text">
+                  <i class="fas fa-smog"></i>
+                    18°C 
+                </div>            
+          </div>  
+        </div>
+        <UnitConversion/>
+        <MoreInfo/>
+        </div>
+       
+     
+  );} else {return<div>{searchForm}
+    <Footer />
+  </div>}
+
+
+
 }
