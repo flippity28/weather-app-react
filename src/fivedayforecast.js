3f97cb267e3cfeb36dc58281f05fa4625cebf056
flippity28 <70317116+flippity28@users.noreply.github.com>
2020-10-28 12:21:19 +0000

    further css formatting

--- src/fivedayforecast.js
+++ src/fivedayforecast.js
@@ -9,19 +9,19 @@ import "./dayforecast.css"
 export default function FiveDayForecast() {
   return (
     <div className="card-deck row-cols-1 five-day-forecast">
-      <div className="card five-day col-sm-6">
+      <div className="card card-one five-day col-sm-6">
         <DayOne />
       </div>
-      <div className="card five-day col-sm-6">
+      <div className="card card-two five-day col-sm-6">
         <DayTwo />
       </div>
-      <div className="card five-day col-sm-6">
+      <div className="card card-three five-day col-sm-6">
         <DayThree />
       </div>
-      <div className="card five-day col-sm-6">
+      <div className="card card-four five-day col-sm-6">
         <DayFour />
       </div>
-      <div className="card five-day col-sm-6">
+      <div className="card card-five five-day col-sm-6">
         <DayFive />
       </div>
     </div>
